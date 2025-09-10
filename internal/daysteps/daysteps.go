8469254b8d6b36aa8acd65f042d647cc80952cab
gamburg <ulsk0073@gmail.com>
2025-09-10 11:02:16 +0400

    first_commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,12 @@
 package daysteps
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -12,9 +17,39 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	parseData := strings.Split(data, ",")
+	if len(parseData) != 2 {
+		return 0, 0, fmt.Errorf("there are not enough parameters")
+	}
+	stepCount, err := strconv.Atoi(parseData[0])
+	if err != nil {
+		return 0, 0, fmt.Errorf("error in number of steps")
+	} else if stepCount == 0 {
+		return 0, 0, fmt.Errorf("the number of steps is zero")
+	}
+	walkDuration, err := time.ParseDuration(parseData[1])
+	if err != nil {
+		return 0, 0, fmt.Errorf("error in the duration of the walk")
+	} else if walkDuration == 0 {
+		return 0, 0, fmt.Errorf("the walking time is zero")
+	}
+	return stepCount, walkDuration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	stepCount, walkDuration, err := parsePackage(data)
+	if err != nil {
+		fmt.Println("something went wrong", err)
+		return ""
+	} else if stepCount == 0 {
+		fmt.Println("something went wrong", err)
+		return ""
+	}
+	distance := float64(stepCount) * stepLength
+	distance /= mInKm
+	calories, err := spentcalories.WalkingSpentCalories(stepCount, weight, height, walkDuration)
+	if err != nil {
+		fmt.Println("something went wrong", err)
+	}
+	return (fmt.Sprintf("Количество шагов: %d.\nДистанция составила %f км.\nВы сожгли %f ккал.", stepCount, distance, calories))
 }
